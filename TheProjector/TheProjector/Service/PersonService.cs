e452219bc5c27971233146ddd0da5ff93b5ad489
jeremy06 <jeremyjeneva@yahoo.com>
2013-10-18 15:35:59 +0800

    Add

---
+++ TheProjector/TheProjector/Service/PersonService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheProjector.DAO;
+using TheProjector.Models;
+
+namespace TheProjector.Service
+{
+    public class PersonService
+    {
+
+        ProjectDAL projector = new ProjectDAL();
+
+        public Object CreateUserResponse(PersonModel person)
+        {
+            var status = projector.CreatePerson(person);
+
+            var data = new { status };
+
+            return data;
+        }
+
+        public Object AuthenticateUserResponse(PersonModel person)
+        {
+            var status = projector.AuthenticateUser(person);
+
+            var data = new { status };
+
+            return data;
+        }
+
+    }
+}
